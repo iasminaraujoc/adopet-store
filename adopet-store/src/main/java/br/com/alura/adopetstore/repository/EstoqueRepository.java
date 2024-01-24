8fe1ca0f8964a27c7220a9980ddff5b20ceeb7c2
iasminaraujoc <iasmincorreaaraujo@gmail.com>
2024-01-24 10:43:07 -0300

    Corrigindo exclusão lógica

--- adopet-store/src/main/java/br/com/alura/adopetstore/repository/EstoqueRepository.java
+++ adopet-store/src/main/java/br/com/alura/adopetstore/repository/EstoqueRepository.java
@@ -9,6 +9,9 @@ import java.util.List;
 
 public interface EstoqueRepository extends JpaRepository<Estoque, Long> {
     Estoque findByProdutoId(Long idProduto);
+
+    @Query("SELECT e FROM Estoque e WHERE e.produto.ativo=true")
+    List<Estoque> findAll();
     @Query("SELECT e.produto FROM Estoque e WHERE e.quantidade = 0")
     List<Produto> produtosComEstoqueZerado();
 }
--- adopet-store/src/main/java/br/com/alura/adopetstore/repository/ProdutoRepository.java
+++ adopet-store/src/main/java/br/com/alura/adopetstore/repository/ProdutoRepository.java
@@ -1,8 +1,13 @@
 package br.com.alura.adopetstore.repository;
 
 import br.com.alura.adopetstore.model.Produto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
      boolean existsByNomeIgnoringCase(String nome);
+     @Query("SELECT p FROM Produto p WHERE p.ativo=true")
+     Page<Produto> findAll(Pageable pageable);
 }
